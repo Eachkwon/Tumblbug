0e8f24ceac4bd460f789a1e519eb72fa5203ac0d
KimHyeon <hye07on11@gmail.com>
2022-08-01 12:30:44 +0900

    프로젝트 리스트 조회 및 프로젝트 생성 기능 테스트 코드 작성

--- src/main/java/com/example/tumblbug/controller/ProjectController.java
+++ src/main/java/com/example/tumblbug/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package com.example.tumblbug.controller;
 
-import com.example.tumblbug.dto.AllProjectResponseDto;
 import com.example.tumblbug.dto.ProjectDetailResponseDto;
-import com.example.tumblbug.dto.ProjectRequesetDto;
+import com.example.tumblbug.dto.ProjectRequestDto;
+import com.example.tumblbug.dto.ProjectsByCategoryResponseDto;
+import com.example.tumblbug.security.UserDetailsImpl;
 import com.example.tumblbug.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +17,22 @@ public class ProjectController {
 
     private final ProjectService projectService;
 
+    // 프로젝트 리스트 조회
     @GetMapping("/api/projects")
-    public List<AllProjectResponseDto> getAllProjects(@RequestParam String category) {
-        return projectService.getAllProjects();
+    public List<ProjectsByCategoryResponseDto> getProjects(@RequestParam String category) {
+        return projectService.getProjectsByCategory(category);
     }
-    
+
     // 프로젝트 상세정보 조회
     @GetMapping("/api/projects/{projectId}")
     public ProjectDetailResponseDto getProject(@PathVariable Long projectId) {
         return projectService.getProjectDetail(projectId);
     }
 
+    // 프로젝트 생성
     @PostMapping("/api/projects")
-    public void createProject(@RequestBody ProjectRequesetDto projectRequesetDto) {
-        projectService.createProject(projectRequesetDto);
+    public void postProject(@RequestBody ProjectRequestDto projectRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        projectService.createProject(projectRequestDto, userDetails.getUser());
     }
 
 }
